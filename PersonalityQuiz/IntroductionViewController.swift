b6fb6b685337af40d66dde079e70e89e50e9f5dc
BP88 <balmes.pavlov@gmail.com>
2019-02-24 18:21:57 -0500

    Initial commit

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Balmes Pavlov on 1/13/19.
+//  Copyright © 2019 Balmes Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Balmes Pavlov on 1/13/19.
+//  Copyright © 2019 Balmes Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    var responses: [Answer]!
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        
+        calculatePersonalityResult()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
